49e7d6938127a669c6e7d9110adfa6c01e2555f7
fedexx1 <126490436+fedexx1@users.noreply.github.com>
2023-03-17 15:45:21 -0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,10 @@
+//GRID
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
+
